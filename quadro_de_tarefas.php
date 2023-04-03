07b7bf830a508dc7c11fd0ec9b57d307ad8ed24c
Ana Vitoria <97069033+anavitoriagoess@users.noreply.github.com>
2023-04-03 01:08:18 +0000

    css button logout

--- quadro_de_tarefas.php
+++ quadro_de_tarefas.php
@@ -3,5 +3,19 @@ session_start();
 include('verifica_login.php');
 ?>
 
-<h2>Olá, <?php echo $_SESSION['nome'];?></h2>
-<h2><a href="logout.php">Sair</a></h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="to-do.css">
+
+    <title>Quadro de tarefas</title>
+</head>
+<body>
+    <h2>Olá, <?php echo $_SESSION['nome'];?></h2>
+    <button><a href="logout.php" class="btn-logout" >Sair</a></button>
+</body>
+</html>
