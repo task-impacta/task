833b04ae50e3ff1a26a21dbd46a5f9aee6532a3d
Ana <anavitoria.goes@hotmail.com>
2023-04-21 17:12:47 -0300

    add notification bar

--- cadastro.php
+++ cadastro.php
@@ -3,78 +3,91 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <link rel="stylesheet" href="style.css" />
-    <title>Cadastre-se!</title>
-  </head>
-  <body>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<div class="container">
-      <h1>Cadastre-se</h1>
-      <?php 
-    if(isset($_SESSION['status_cadastro'])):
+  <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/css/toastr.css" rel="stylesheet" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/js/toastr.js"></script>
+
+  <link rel="stylesheet" href="style.css" />
+  <title>Cadastre-se</title>
+</head>
+
+<body>
+
+  <div class="container">
+    <h1>Cadastre-se</h1>
+    <?php
+    if (isset($_SESSION['status_cadastro'])) :
     ?>
-    <div class="sucesso-verde">
-      <p>Cadastro efetuado com sucesso!</p>
-    </div>
-    <?php 
-    
-    unset($_SESSION['status_cadastro']);
+      <script type="text/javascript">
+        toastr.success('Cadastro efetuado com sucesso!', {
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+      </script>
+      <?php
+      header("refresh:3;URL=login.php");
+      ?>
+    <?php
+      unset($_SESSION['status_cadastro']);
     endif;
     ?>
 
-    <?php 
-    if(isset($_SESSION['usuario_existe'])):
+    <?php
+    if (isset($_SESSION['usuario_existe'])) :
     ?>
-    <div class="notificacao-azul">
-        <p>Ops... Já existe um usuário cadastrado para o e-mail informado. Por favor, altere os dados e tente novamente.</p>
-    </div>
-    <?php 
+      <script type="text/javascript">
+        toastr.info('Ops... Já existe um usuário cadastrado para o e-mail informado. Por favor, altere os dados e tente novamente.', {
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+      </script>
+    <?php
     endif;
     unset($_SESSION['usuario_existe']);
     ?>
-      <form method="POST" action="cadastrar.php">
-    
-        <h2>Preencha os campos necessários para se cadastrar</h2>
 
-        <fieldset>
-          <legend>Nome completo:*</legend>
-          <input type="text" name="nome" id="nameForm" minlength="3" required />
-        </fieldset>
+    <form method="POST" action="cadastrar.php">
 
-        <fieldset>
-          <legend>E-mail:*</legend>
-          <input
-            type="email"
-            name="email"
-            id="emaileForm"
-            minlength="3"
-            required
-          />
-        </fieldset>
+      <h2>Preencha os campos necessários para se cadastrar</h2>
 
-        <fieldset>
-          <legend>Senha:*</legend>
-          <input
-            type="password"
-            name="senha"
-            id="passwordForm"
-            minlength="3"
-            required
-          />
-        </fieldset>
+      <fieldset>
+        <legend>Primeiro nome: *</legend>
+        <input type="text" name="nome" id="nameForm" minlength="3" required />
+      </fieldset>
 
-        <footer>
-          <span class="hasAccount"
-            >Já possui uma conta? <a href="index.php">Clique aqui</a> e faça seu
-            login</span>
-            <button type="submit">Cadastrar</button>
-        </footer>
-      </form>
-    </div>
-  </body>
+      <fieldset>
+        <legend>E-mail:*</legend>
+        <input type="email" name="email" id="emaileForm" minlength="3" required />
+      </fieldset>
+
+      <fieldset>
+        <legend>Senha:*</legend>
+        <input type="password" name="senha" id="passwordForm" minlength="3" required />
+      </fieldset>
+
+      <footer>
+        <span class="hasAccount">Já possui uma conta? <a href="index.php">Clique aqui</a> e faça seu
+          login</span>
+        <button type="submit">Cadastrar</button>
+      </footer>
+    </form>
+  </div>
+</body>
+
 </html>
--- index.php
+++ index.php
@@ -3,53 +3,63 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="style.css" />
-    <title>Login</title>
-  </head>
-  <body>
-   
-    <div class="container">
-      
-      <h1>Faça seu login</h1>
 
-      <?php
-         if(isset($_SESSION['nao_autenticado'])):
-         ?>
-          <div class="erro-vermelho">
-            <p>ERRO: Usuário ou senha inválidos.</p>
-          </div>
-          <?php
-          endif;
-          unset($_SESSION['nao_autenticado']);
-          ?>
-      
-      <form action="login.php" method="POST">
-        <fieldset>
-          <legend>Email:*</legend>
-          <input type="text" name="email" id="email" minlength="3" required />
-        </fieldset>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <fieldset>
-          <legend>Senha:*</legend>
-          <input
-            type="password"
-            name="senha"
-            id="passwordForm"
-            minlength="3"
-            required
-          />
-        </fieldset>      
-        
-        <footer>
-          <span class="hasAccount"
-            >Não possui cadastro? <a href="cadastro.php">Cadastre-se</a></span>
-          <button type="submit">Entrar</button>
-        </footer>
-      </form>
-    </div>
-  </body>
+  <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/css/toastr.css" rel="stylesheet" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/js/toastr.js"></script>
+
+  <link rel="stylesheet" href="style.css" />
+  <title>Login</title>
+</head>
+
+<body>
+
+  <div class="container">
+
+    <h1>Faça seu login</h1>
+
+    <?php
+    if (isset($_SESSION['nao_autenticado'])) :
+    ?>
+
+      <script type="text/javascript">
+        toastr.error('Usuário ou senha inválidos.', {
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+      </script>
+    <?php
+    endif;
+    unset($_SESSION['nao_autenticado']);
+    ?>
+
+    <form action="login.php" method="POST">
+      <fieldset>
+        <legend>Email:*</legend>
+        <input type="text" name="email" id="email" minlength="3" required />
+      </fieldset>
+
+      <fieldset>
+        <legend>Senha:*</legend>
+        <input type="password" name="senha" id="passwordForm" minlength="3" required />
+      </fieldset>
+
+      <footer>
+        <span class="hasAccount">Não possui cadastro? <a href="cadastro.php">Cadastre-se</a></span>
+        <button type="submit">Entrar</button>
+      </footer>
+    </form>
+  </div>
+</body>
+
 </html>
