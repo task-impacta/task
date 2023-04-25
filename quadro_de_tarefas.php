cb5cb5195aaa634f022ee67f710847f8a62da9cb
Ana <anavitoria.goes@hotmail.com>
2023-04-25 16:34:36 -0300

    css adjustment

--- quadro_de_tarefas.php
+++ quadro_de_tarefas.php
@@ -3,7 +3,7 @@ session_start();
 include('conexao.php');
 include('verifica_login.php');
 include('func.php');
-if(!loggedin()){
+if (!loggedin()) {
     header("location:login.php");
 }
 ?>
@@ -17,21 +17,19 @@ if(!loggedin()){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="to-do.css">
+    <link href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous" rel="stylesheet">
 
     <title>Quadro de tarefas</title>
 </head>
 
 <body>
-    <div>
-        <center id = "todo"></center>
-    </div>
     <div class="position-btn">
         <a href="logout.php" class="btn-logout">Sair</a>
     </div>
 
     <div class="content">
         <header class="card-header">
-            <h2>Olá, <?php echo $_SESSION['nome']; ?>! O que você quer fazer hoje? </h2>
+            <h2 class="card-header-title">Olá, <?php echo $_SESSION['nome']; ?>! O que você quer fazer hoje? </h2>
 
             <form method="POST">
                 <input type="text" name="tarefa" class="new-task" placeholder="Escreva o que você quer fazer..." />
@@ -39,64 +37,28 @@ if(!loggedin()){
             </form>
         </header>
         <article class="card-content">
-            <div>
-                <h2 class="card-content-title">LISTA DE TAREFAS A FAZER:</h2>
-            </div>
-            <div>
-                <div class="card-content-task">
-                    <div class="card-content-task-message">
-                        <label>Ir a academia</label>
-                    </div>
-                    <div class="card-content-task-icon">
-                        <input type="checkbox" class="checkbox-round" />
-                        <img class="img-trash" src="C:\Users\jrica\Downloads\HTML_Ricardo\trash_icon.svg">
-                        <img class="img-pencil" src="C:\Users\jrica\Downloads\HTML_Ricardo\pencil_write_icon.svg">
-                    </div>
+            <h2 class="card-content-title">Lista de tarefas a fazer:</h2>
+
+            <div class="card-content-task">
+                <label>Ir a academia</label>
+                <div class="card-content-task-icon">
+                    <input type="checkbox" class="checkbox-round" />
+                    <button class="btn-icon">
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
+                    <button class="btn-icon">
+                        <i class="fas fa-pen"></i>
+                    </button>
                 </div>
-
-                <div class="card-content-task">
-                    <div class="card-content-task-message">
-                        <label>Estudar</label>
-                    </div>
-                    <div class="card-content-task-icon">
-                        <input type="checkbox" class="checkbox-round" />
-                        <img class="img-trash" src="C:\Users\jrica\Downloads\HTML_Ricardo\trash_icon.svg">
-                        <img class="img-pencil" src="C:\Users\jrica\Downloads\HTML_Ricardo\pencil_write_icon.svg">
-                    </div>
-                </div>
-
-                <div class="card-content-task">
-                    <div class="card-content-task-message">
-                        <label>Ler um livro</label>
-                    </div>
-                    <div class="card-content-task-icon">
-                        <input type="checkbox" class="checkbox-round" />
-                        <img class="img-trash" src="C:\Users\jrica\Downloads\HTML_Ricardo\trash_icon.svg">
-                        <img class="img-pencil" src="C:\Users\jrica\Downloads\HTML_Ricardo\pencil_write_icon.svg">
-                    </div>
-                </div>
-
-                <div class="card-content-task">
-                    <div class="card-content-task-message">
-                        <label>Escutar podcast</label>
-                    </div>
-                    <div class="card-content-task-icon">
-                        <input type="checkbox" class="checkbox-round" />
-                        <img class="img-trash" src="C:\Users\jrica\Downloads\HTML_Ricardo\trash_icon.svg">
-                        <img class="img-pencil" src="C:\Users\jrica\Downloads\HTML_Ricardo\pencil_write_icon.svg">
-                    </div>
-                </div>
-
-
-
             </div>
         </article>
 
     </div>
 
 </body>
+
 </html>
 <?php
-    $fk_user = $_SESSION['fk_user'];
-    getTodoItems($fk_user);
- ?>
+$fk_user = $_SESSION['fk_user'];
+getTodoItems($fk_user);
+?>
