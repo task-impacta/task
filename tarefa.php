e60ca83645aac7a825fe4e28d978d242789a3e3b
josesimoes123 <127610255+josesimoes123@users.noreply.github.com>
2023-04-24 22:25:35 -0300

    Add files via upload

--- tarefa.php
+++ tarefa.php
@@ -1,4 +1,3 @@
-
 <?php 
     $con = new PDO ("mysql:host=localhost;dbname=tarefas","root", "");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -40,38 +39,90 @@
     
 
 ?>
+
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="tarefa.css" />
-    <title>Lista de Tarefas</title>
 
+    <link rel="stylesheet" href="to-do.css">
+
+    <title>Quadro de tarefas</title>
 </head>
+
 <body>
-    <form method="post">
-        Nova Tarefa: <input type="text" name="tarefa" />
-        <input type="submit" value ="Incluir" />
-    </form>
-    <div class="lista">
-        <ul>
-            <?php foreach($lista as $item): ?>
-                <li <?= $item['concluida']?'class="concluida"':''?>> 
-                <?=$item['descricao']?>
-                    <?php if(!$item['concluida']):?>
-                        <a href ="?concluir=<?=$item ['id']?>">Concluir]</a>
-                    <?php endif;?>
-                        <a href ="?excluir=<?=$item ['id']?>">[Excluir]</a>
-            </li>
-            <?php endforeach; ?>
-
-
-
-        
-        </ul>
+    <div class="position-btn">
+        <a href="logout.php" class="btn-logout">Sair</a>
     </div>
-    
+
+    <div class="content">
+        <header class="card-header">
+            <h2 class="card-header-title">Olá, <?php echo $_SESSION['nome']; ?>! O que você quer fazer hoje? </h2>
+
+            <form method="POST">
+                <input type="text" name="tarefa" class="new-task" placeholder="Escreva o que você quer fazer..." />
+                <button type="submit" value="incluir" class="add-task">Criar tarefa</button>
+            </form>
+        </header>
+        <article class="card-content">
+            <div>
+                <h2 class="card-content-title">LISTA DE TAREFAS A FAZER:</h2>
+            </div>
+            <div>
+                <div class="card-content-task">
+                    <div class="card-content-task-message">
+                        <label>Ir a academia</label>
+                    </div>
+                    <div class="card-content-task-icon">
+                        <input type="checkbox" class="checkbox-round" />
+                        <img class="img-trash" src="C:\Users\jrica\Downloads\HTML_Ricardo\trash_icon.svg">
+                        <img class="img-pencil" src="C:\Users\jrica\Downloads\HTML_Ricardo\pencil_write_icon.svg">
+                    </div>
+                </div>
+
+                <div class="card-content-task">
+                    <div class="card-content-task-message">
+                        <label>Estudar</label>
+                    </div>
+                    <div class="card-content-task-icon">
+                        <input type="checkbox" class="checkbox-round" />
+                        <img class="img-trash" src="C:\Users\jrica\Downloads\HTML_Ricardo\trash_icon.svg">
+                        <img class="img-pencil" src="C:\Users\jrica\Downloads\HTML_Ricardo\pencil_write_icon.svg">
+                    </div>
+                </div>
+
+                <div class="card-content-task">
+                    <div class="card-content-task-message">
+                        <label>Ler um livro</label>
+                    </div>
+                    <div class="card-content-task-icon">
+                        <input type="checkbox" class="checkbox-round" />
+                        <img class="img-trash" src="C:\Users\jrica\Downloads\HTML_Ricardo\trash_icon.svg">
+                        <img class="img-pencil" src="C:\Users\jrica\Downloads\HTML_Ricardo\pencil_write_icon.svg">
+                    </div>
+                </div>
+
+                <div class="card-content-task">
+                    <div class="card-content-task-message">
+                        <label>Escutar podcast</label>
+                    </div>
+                    <div class="card-content-task-icon">
+                        <input type="checkbox" class="checkbox-round" />
+                        <img class="img-trash" src="C:\Users\jrica\Downloads\HTML_Ricardo\trash_icon.svg">
+                        <img class="img-pencil" src="C:\Users\jrica\Downloads\HTML_Ricardo\pencil_write_icon.svg">
+                    </div>
+                </div>
+
+
+
+            </div>
+        </article>
+
+    </div>
+
 </body>
+
 </html>
