a91a98bcc593aea4ac5e1696b57a21c76c510e03
Ana <anavitoria.goes@hotmail.com>
2023-04-25 23:05:06 -0300

    add functions to delete, finish, unfinished

--- func.php
+++ func.php
@@ -1,27 +1,33 @@
 <?php
-if (isset($_POST['Delete'])) {
-    if (!empty($_POST['check_list'])) {
-        $tasks = $_POST['check_list'];
-        $length = count($tasks);
-        for ($i = 0; $i < $length; $i++) {
-            deleteTodoItem($_SESSION['nome'], $tasks[$i]);
-        }
-    }
-} else if (isset($_POST['Save'])) {
+
+// Excluir tarefas
+if (isset($_GET['del_task'])) {
+    $id_tarefa = $_GET['del_task'];
+
     $conn = connectdatabase();
-    $sql = "UPDATE tarefas SET finalizado = 'nao";
+    $sql = "DELETE FROM tarefas WHERE (id_tarefa = '" . $id_tarefa . "');";
     $result = mysqli_query($conn, $sql);
     mysqli_close($conn);
+}
 
-    if (!empty($_POST['check_list'])) {
-        $tasks = $_POST['check_list'];
-        $length = count($tasks);
-        if ($length > 0) {
-            for ($i = 0; $i < $length; $i++) {
-                updateDone($tasks[$i]);
-            }
-        }
-    }
+// Marcar tarefas como concluída
+if (isset($_GET['finish'])) {
+    $id_tarefa = $_GET['finish'];
+
+    $conn = connectdatabase();
+    $sql = "UPDATE tarefas SET finalizado = 'sim' WHERE (id_tarefa = '" . $id_tarefa  . "');";
+    $result = mysqli_query($conn, $sql);
+    mysqli_close($conn);
+}
+
+// Desmarcar tarefas como concluída 
+if (isset($_GET['unfineshed'])) {
+    $id_tarefa = $_GET['unfineshed'];
+
+    $conn = connectdatabase();
+    $sql = "UPDATE tarefas SET finalizado = 'nao' WHERE (id_tarefa = '" . $id_tarefa  . "');";
+    $result = mysqli_query($conn, $sql);
+    mysqli_close($conn);
 }
 
 function connectdatabase()
@@ -34,33 +40,6 @@ function loggedin()
     return isset($_SESSION['nome']);
 }
 
-
-// function userexist($nome)
-// {
-//     $conn = connectdatabase();
-//     $sql = "SELECT * FROM quadro_de_tarefas.usuario WHERE nome = '" . $nome . "'";
-//     $result = mysqli_query($conn, $sql);
-//     mysqli_close($conn);
-
-//     if (!$result || mysqli_num_rows($result) == 0) {
-//         return false;
-//     }
-//     return true;
-// }
-
-// function validuser($nome, $senha, $email, $fk_user)
-// {
-//     $conn = connectdatabase();
-//     $sql = "SELECT * FROM quadro_de_tarefas.usuario WHERE nome = '" . $nome . "'AND senha = '" . $senha . "'AND email = '" . $email . "'AND fk_user = '" . $fk_user . "'";
-//     $result = mysqli_query($conn, $sql);
-//     mysqli_close($conn);
-
-//     if (!$result || mysqli_num_rows($result) == 0) {
-//         return false;
-//     }
-//     return true;
-// }
-
 function error()
 {
     if (isset($_SESSION['error'])) {
@@ -68,46 +47,6 @@ function error()
         unset($_SESSION['error']);
     }
 }
-
-// function updatepassword($nome, $senha)
-// {
-//     $conn = connectdatabase();
-//     $sql = "UPDATE quadro_de_tarefas.usuario SET senha = '" . $senha . "' WHERE nome = '" . $nome . "';";
-//     $result = mysqli_query($conn, $sql);
-
-//     $_SESSION['error'] = "<br> &nbsp; Sua senha foi alterada com sucesso!";
-//     header('location:todo.php');
-// }
-
-// function deleteaccount($nome)
-// {
-//     $conn = connectdatabase();
-//     $sql = "DELETE FROM quadro_de_tarefas.tarefas WHERE nome = '" . $nome . "';";
-//     $result = mysqli_query($conn, $sql);
-
-//     $sql = "DELETE FROM quadro_de_tarefas.usuario WHERE nome = '" . $nome . "';";
-//     $result = mysqli_query($conn, $sql);
-
-//     $_SESSION['error'] = "&nbsp; Conta deletada";
-//     unset($_SESSION['nome']);
-//     header('location:login.php');
-// }
-
-// function isValid($nome, $senha, $email, $fk_user)
-// {
-
-//     $conn = connectdatabase();
-//     if (validuser($nome, $senha, $email, $fk_user)) {
-//         $_SESSION["nome"] = $nome;
-//         header('location:todo.php');
-//     } else {
-//         $_SESSION['error'] = "&nbsp; Usuário inválido ou senha inválida";
-//         header('location:login.php');
-//     }
-//     mysqli_close($conn);
-// }
-
-
 
 function getTodoItems($fk_user)
 {
@@ -117,49 +56,34 @@ function getTodoItems($fk_user)
 
     $result = mysqli_query($conn, $sql);
 
-
     if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_assoc($result)) { ?>
 
-            <div class="card-content-task">
-                <?php
-                if ($row['finalizado'] == "sim") { ?>
-                    <label class="finish"><?php echo $row['tarefa']; ?></label>
-                <?php
-                } else { ?>
-                    <label><?php echo $row['tarefa']; ?></label>
-                <?php } ?>
-
-                <div class="card-content-task-icon">
+            <form method='POST'>
+                <div class="card-content-task">
                     <?php
-                    if ($row['finalizado'] == "sim") {
-                        echo "<input type='checkbox' checked class='checkbox-round' name='check_list[]' value='" . $row["id_tarefa"] . "'>";
-                    } else {
-                        echo "<input type='checkbox' class='checkbox-round' name='check_list[]' value='" . $row["id_tarefa"] . "'>";
-                    }
-                    ?>
-                    <button class="btn-icon" type='submit' name='Delete' value='Deletar'>
-                        <i class="fas fa-trash-alt"></i>
-                    </button>
-                    <button class="btn-icon">
-                        <i class="fas fa-pen"></i>
-                    </button>
+                    if ($row['finalizado'] == "sim") { ?>
+                        <label class="finish"><?php echo $row['tarefa']; ?></label>
+                    <?php
+                    } else { ?>
+                        <label><?php echo $row['tarefa']; ?></label>
+                    <?php } ?>
+
+                    <div class="card-content-task-icon">
+                        <?php
+                        if ($row['finalizado'] == "sim") { ?>
+                            <a href="quadro_de_tarefas.php?unfineshed=<?php echo $row['id_tarefa'] ?>"><i class="fas fa-check-circle"></i></a>
+                        <?php
+                        } else { ?>
+                            <a href="quadro_de_tarefas.php?finish=<?php echo $row['id_tarefa'] ?>"><i class="far fa-circle"></i></a>
+                        <?php
+                        }
+                        ?>
+                        <a href="quadro_de_tarefas.php?del_task=<?php echo $row['id_tarefa'] ?>"><i class="fas fa-trash-alt"></i></a>
+                        <a href="quadro_de_tarefas.php?edit=<?php echo $row['id_tarefa'] ?>"><i class="fas fa-pen"></i></a>
+                    </div>
                 </div>
-            </div>
-
-
-            <!-- echo "<form method='POST'>";
-                echo "<label>" . $row['tarefa'] . "</label>";
-                if ($row['finalizado'] == "sim") {
-                echo "<input type='checkbox' checked class='largerCheckbox' name='check_list[]' value='" . $row["id_tarefa"] . "'>";
-                } else {
-                echo "<input type='checkbox' class='largerCheckbox' name='check_list[]' value='" . $row["id_tarefa"] . "'>";
-                }
-
-                echo "<input type='submit' name='Delete' value='Deletar' />";
-                echo "<input type='submit' name='Save' value='Concluir' />";
-
-                echo "</form>"; -->
+            </form>
 <?php
         }
         return $row;
@@ -167,28 +91,10 @@ function getTodoItems($fk_user)
     mysqli_close($conn);
 }
 
-
-
 function addTodoItem($fk_user, $todo_text)
 {
     $conn = connectdatabase();
     $sql = "INSERT INTO tarefas (tarefa, finalizado, fk_user) VALUES ('" . $todo_text . "','nao','" . $fk_user . "');";
-    $result = mysqli_query($conn, $sql);
-    mysqli_close($conn);
-}
-
-function deleteTodoItem($fk_user, $todo_id)
-{
-    $conn = connectdatabase();
-    $sql = "DELETE FROM tarefas WHERE id_tarefa = " . $todo_id . " and fk_user = '" . $fk_user . "';";
-    $result = mysqli_query($conn, $sql);
-    mysqli_close($conn);
-}
-
-function updateDone($todo_id)
-{
-    $conn = connectdatabase();
-    $sql = "UPDATE tarefas SET finalizado = 'sim' WHERE (id_tarefa = '" . $todo_id . "');";
     $result = mysqli_query($conn, $sql);
     mysqli_close($conn);
 }
